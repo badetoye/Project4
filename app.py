7a33502d27ff657db6d1618e89f6b3c3fb2e0c40
Bolaji Adetoye <40571508+badetoye@users.noreply.github.com>
2020-09-01 21:59:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,28 @@ def predict():
         "0":396.9
         },
         "LSTAT":{
-      
+       "0":4.98
+        }
+        
+        result looks like:
+        { "prediction": [ <val> ] }
+        
+        """
+    
+    # Logging the input payload
+    json_payload = request.json
+    LOG.info(f"JSON payload: \n{json_payload}")
+    inference_payload = pd.DataFrame(json_payload)
+    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+    # scale the input
+    scaled_payload = scale(inference_payload)
+    # get an output prediction from the pretrained model, clf
+    prediction = list(clf.predict(scaled_payload))
+    # TO DO:  Log the output prediction value
+    print(f"The log message is.\n{prediction}")
+    return jsonify({'prediction': prediction})
+
+if __name__ == "__main__":
+    # load pretrained model as clf
+    clf = joblib.load("./model_data/boston_housing_prediction.joblib")
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
